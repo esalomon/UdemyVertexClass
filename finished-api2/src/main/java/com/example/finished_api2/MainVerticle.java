c1554b88c9f79ba50b3718c5c48b22183b961670
Efrain Salomon <efrain_salomon@yahoo.com.mx>
2023-07-16 18:40:13 -0400

    [finished-api2] The MainVerticle was updated with the main method and it retrieves the configuration from 'config/config.json' file. Also, the finished-api2 main.run.xml configuration file was created to start the project using the main method.

--- finished-api2/src/main/java/com/example/finished_api2/MainVerticle.java
+++ finished-api2/src/main/java/com/example/finished_api2/MainVerticle.java
@@ -1,11 +1,31 @@
 package com.example.finished_api2;
 
+import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MainVerticle extends AbstractVerticle {
+
+    public static void main(String... args) {
+
+        Vertx vertx = Vertx.vertx();
+
+        ConfigRetriever configRetriever = ConfigRetriever.create(vertx);
+        configRetriever.getConfig(config -> { // Use config/config.json from resources/classpath
+
+            if (config.succeeded()) {
+                JsonObject configJson = config.result();
+                log.info("Retrieved properties from 'config/config.json' \n{}", configJson.encodePrettily());
+                DeploymentOptions options = new DeploymentOptions().setConfig(configJson);
+                vertx.deployVerticle(new MainVerticle(), options);
+            }
+        });
+    }
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
