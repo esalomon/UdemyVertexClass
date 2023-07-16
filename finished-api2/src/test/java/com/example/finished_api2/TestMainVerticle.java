ebf792e7d3ddd46211746a29287077ab6dbd75b9
Efrain Salomon <efrain_salomon@yahoo.com.mx>
2023-07-16 13:34:00 -0400

    [finished-api2] The lombok dependency was added. Also, the MainVerticle was updated with the @Slf4j annotation.

--- finished-api2/src/test/java/com/example/finished_api2/TestMainVerticle.java
+++ finished-api2/src/test/java/com/example/finished_api2/TestMainVerticle.java
@@ -8,15 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(VertxExtension.class)
-public class TestMainVerticle {
+class TestMainVerticle {
 
-  @BeforeEach
-  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
-  }
+    @BeforeEach
+    void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+        vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+    }
 
-  @Test
-  void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
-    testContext.completeNow();
-  }
+    @Test
+    void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
+        testContext.completeNow();
+    }
 }
