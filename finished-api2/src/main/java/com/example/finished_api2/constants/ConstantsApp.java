4f93a7d67479b003c4d873a18a58acec3f6f421e
Efrain Salomon <efrain_salomon@yahoo.com.mx>
2023-07-17 11:25:51 -0400

    [finished-api2] The MongoDBVerticle was updated to register an event bus consumer message.

---
+++ finished-api2/src/main/java/com/example/finished_api2/constants/ConstantsApp.java
@@ -0,0 +1,10 @@
+package com.example.finished_api2.constants;
+
+public class ConstantsApp {
+
+
+    public static final String MONGO_DB_MESSAGE = "com.example.mongo-db";
+
+    private ConstantsApp() {
+    }
+}
--- finished-api2/src/main/java/com/example/finished_api2/resources/MongoDBVerticle.java
+++ finished-api2/src/main/java/com/example/finished_api2/resources/MongoDBVerticle.java
@@ -2,7 +2,13 @@ package com.example.finished_api2.resources;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 import lombok.extern.slf4j.Slf4j;
+
+import static com.example.finished_api2.constants.ConstantsApp.MONGO_DB_MESSAGE;
 
 @Slf4j
 public class MongoDBVerticle extends AbstractVerticle {
@@ -11,7 +17,34 @@ public class MongoDBVerticle extends AbstractVerticle {
     public void start(Promise<Void> startPromise) throws Exception {
 
         log.info("The {} verticle is starting...", MongoDBVerticle.class.getSimpleName());
+        registerConsumer(vertx);
         startPromise.complete();
+    }
+
+    public void registerConsumer(Vertx vertx) {
+
+        log.info("The MongoDB Event Bus consumer is register.");
+        vertx.eventBus().consumer(MONGO_DB_MESSAGE, message -> {
+
+            log.info("Received message: {}", Json.encodePrettily(message.body()));
+            JsonObject inputJson = new JsonObject(message.body().toString());
+
+            if (inputJson.getString("command").equals("get-product-list")) {
+                getProductList(message);
+            }
+            else {
+                String errorMessage = "The received message was not processed.";
+                log.error(errorMessage);
+                message.fail(500, errorMessage);
+            }
+        });
+    }
+
+    private void getProductList(Message<Object> message) {
+
+        var response = new JsonObject()
+            .put("message", "get product list was executed in mongo class");
+        message.reply(response);
     }
 
     @Override
