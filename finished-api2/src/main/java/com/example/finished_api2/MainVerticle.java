57713878ca1ee14a70c429adaeba944f26b557d0
Efrain Salomon <efrain_salomon@yahoo.com.mx>
2023-07-17 07:02:19 -0400

    [finished-api2] The MainVerticle's main configuration retriever was moved to the start() method. Now the launcher or the main configuration can be used.

--- finished-api2/src/main/java/com/example/finished_api2/MainVerticle.java
+++ finished-api2/src/main/java/com/example/finished_api2/MainVerticle.java
@@ -14,6 +14,11 @@ public class MainVerticle extends AbstractVerticle {
     public static void main(String... args) {
 
         Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new MainVerticle());
+    }
+
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
 
         ConfigRetriever configRetriever = ConfigRetriever.create(vertx);
         configRetriever.getConfig(config -> { // Use config/config.json from resources/classpath
@@ -22,21 +27,22 @@ public class MainVerticle extends AbstractVerticle {
                 JsonObject configJson = config.result();
                 log.info("Retrieved properties from 'config/config.json' \n{}", configJson.encodePrettily());
                 DeploymentOptions options = new DeploymentOptions().setConfig(configJson);
-                vertx.deployVerticle(new MainVerticle(), options);
+                configHttpServer(startPromise, options);
             }
         });
     }
 
-    @Override
-    public void start(Promise<Void> startPromise) throws Exception {
+    private void configHttpServer(Promise<Void> startPromise, DeploymentOptions options) {
+
+        Integer port = options.getConfig().getInteger("http.port");
         vertx.createHttpServer().requestHandler(req -> {
             req.response()
                 .putHeader("content-type", "text/plain")
                 .end("Hello from Vert.x!");
-        }).listen(8888, http -> {
+        }).listen(port, http -> {
             if (http.succeeded()) {
                 startPromise.complete();
-                log.info("HTTP server started on port 8888");
+                log.info("HTTP server started on port: {}", port);
             } else {
                 startPromise.fail(http.cause());
             }
